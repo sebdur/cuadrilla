be6d6e03603be0037a199940f806b6aff2e903ae
ignaciobriceno <ignacioob.10@gmail.com>
2020-06-28 18:56:21 -0400

    algun cambio en cotizador

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,22 +2,21 @@ module Spree
 	module CheckoutControllerDecorator
 		def update
 			if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
-        @order.temporary_address = !params[:save_user_address]
-        unless @order.next
-          flash[:error] = @order.errors.full_messages.join("\n")
-          redirect_to(checkout_state_path(@order.state)) && return
-        end
+				@order.temporary_address = !params[:save_user_address]
+				unless @order.next
+					flash[:error] = @order.errors.full_messages.join("\n")
+					redirect_to(checkout_state_path(@order.state)) && return
+				end
 
-        if @order.completed?
-          @current_order = nil
-          flash['order_completed'] = true
-          redirect_to completion_route
-        else
-          redirect_to checkout_state_path(@order.state)
-        end
+				if @order.completed?
+					@current_order = nil
+					flash['order_completed'] = true
+					redirect_to completion_route
+				else
+					redirect_to checkout_state_path(@order.state)
+				end
 
 				if @order.state == "payment"
-
 					commune = @order.bill_address.address2.upcase
 					commune_code = QuoteCost.find_by(name: commune).code
 					#hace split de address1 y elimina ultimo espacio del array
@@ -30,9 +29,9 @@ module Spree
 					contact_email = @order.email
 					@shipping_request = ApiShipping.new.envio(commune, commune_code, name_address, num_address, observation, contact_name, contact_number, contact_email)
 				end
-      else
-        render :edit
-      end
+			else
+				render :edit
+			end
 		end
 	end
 end
