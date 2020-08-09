be9588e2a2b45ecce52ac054edb9ef4d32f4ac33
seb <sebduran@protonmail.com>
2020-08-08 20:58:26 -0400

    messages_controller adjustment

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,11 @@ class MessagesController < Spree::StoreController
   end
 
   def create
+    base_url = Rails.env.production? ? Rails.application.credentials.base_url : 'http://localhost:3000'
     @message = Message.new message_params
     if @message.valid?
       @message.send_simple_message
-      redirect_to root_path
+      redirect_to base_url
       flash[:success] = "Mensaje recibido. Te contactaremos a la brevedad!"
     else
       flash[:warning] = "Hubo un problema enviando tu mensaje. Por favor intenta otra vez."
