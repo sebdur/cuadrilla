b0a2382afc3c8e0b750f1ecfe0c7744732b4eeb3
Rodrigo Ulloa <rodrigo.ulloa22@gmail.com>
2020-08-22 02:32:22 -0400

    fix filter author

--- lib/my_store/product_search.rb
+++ lib/my_store/product_search.rb
@@ -7,14 +7,19 @@ module MyStore
       unless query.blank?
         base_scope = base_scope.like_any([:name, :description], [query])
         if base_scope.blank?
-          author_book_search_id = Spree::ProductProperty.where(property_id: author_id, value: query.upcase).pluck(:product_id)
+          set_query = query.split(' ').join(", ")
+          #author_book_search_id = Spree::ProductProperty.where(property_id: author_id, value: like%set_query.strip.upcase%).pluck(:product_id)
+          # author_book_search_id = Spree::ProductProperty.where(property_id: author_id, "value LIKE ?, '%#{set_query.strip.upcase}%'").pluck(:product_id)
+          # author_book_search_id_set = Spree::ProductProperty.where(property_id: author_id).where("value LIKE ?", "%#{set_query.strip.upcase}% ").pluck(:product_id)
+          set_author_book_search_id = Spree::ProductProperty.where(property_id: author_id)
+          author_book_search_id = set_author_book_search_id.where("value LIKE ?", "%#{set_query.strip.upcase}%").pluck(:product_id)
+
           new_query = []
           author_book_search_id.each do |num|
              new_query << Spree::Product.find_by(id: num).name
           end
-          #base_scope = base_scope.like_any([:name, :description], [new_query])
-          #base_scope = base_scope.where(name: new_query)
           base_scope = Spree::Product.where(name: new_query)
+          byebug
         end
       end
       base_scope
